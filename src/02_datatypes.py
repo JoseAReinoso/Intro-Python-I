f9de147a3b767c660c23c8c7638a66ee8a5390f0
Reinoso <jreinoso@amazon.com>
2020-08-05 19:56:05 -0400

    02_datatypes done

--- src/02_datatypes.py
+++ src/02_datatypes.py
@@ -15,7 +15,13 @@ y = "7"
 
 # YOUR CODE HERE
 
+xyint = x + int(y)
+print(xyint, "converted to integer")
+
 
 # Write a print statement that combines x + y into the string value 57
 
 # YOUR CODE HERE
+
+xystr = str(x) + y
+print(xystr, "converted to string")
